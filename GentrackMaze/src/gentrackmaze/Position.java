8fe8f500b480c4decf5ffb123e89bedb40508986
highlee <55447021+highlee@users.noreply.github.com>
2019-09-17 14:39:38 +0100

    Add files via upload

---
+++ GentrackMaze/src/gentrackmaze/Position.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gentrackmaze;
+
+/**
+ *
+ * @author Z
+ */
+public class Position {
+
+    int row;
+    int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
